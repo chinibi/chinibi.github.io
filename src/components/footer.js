46b4a93ed7125bcf3e2df5c6b88d9d8ad38ec28a
Trevor Pham <trevor.dt.pham@gmail.com>
2016-07-07 11:46:12 -0700

    first draft

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+var Footer = React.createClass({
+  render: function() {
+    return (
+      <div id='footer' className="container text-center">
+        <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank"><span class="fa fa-github"></span></a>
+        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank"><span className="fa fa-linkedin"></span></a>
+      </div>
+    );
+  }
+});
