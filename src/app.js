ed31d8ff389ffea3439708c63723b98d0295174e
Trevor Pham <trevor.dt.pham@gmail.com>
2016-07-07 13:31:36 -0700

    links in header

--- src/app.js
+++ src/app.js
@@ -70,6 +70,11 @@ var Header = React.createClass({
         <div className="container">
           <h2>Hello! My name is Trevor and I'm a web developer.</h2>
         </div>
+
+        <div id="header-links" className="container">
+          <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank"><span className="fa fa-github"></span></a>
+          <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank"><span className="fa fa-linkedin"></span></a>
+        </div>
       </div>
     );
   }
