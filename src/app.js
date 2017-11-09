fdd30cd72bb4d94b5b7080471a0f14492da09f71
chinibi <trevor.dt.pham@gmail.com>
2017-11-08 16:30:57 -0800

    add navigation between three sections of site, refactor to es6

--- src/app.js
+++ src/app.js
@@ -1,82 +1,15 @@
-const projects = [
+const interests = [
   {
-    title: 'Step-By-Step Ordering System for WooCommerce',
-    imageURL: '/images/wc-step-by-step-ordering.png',
-    dates: 'July 2017',
-    website: 'http://stepbystepsys.com',
-    github: 'https://github.com/chinibi/wc-step-by-step-ordering-free',
-    tech: ['PHP', 'WordPress', 'JavaScript', 'WooCommerce'],
+    title: 'PhysicsSims',
+    imageURL: '/images/physics.jpg',
+    dates: 'November 2017',
+    github: 'http://github.com/chinibi/physics-sims',
+    tech: ['JavaScript', 'Canvas'],
     description: (
       <div>
-        <p>Created a step-by-step ordering plugin that extends the WooCommerce WordPress library.
-          Customers are guided through a store by progressing through a number of steps each showing different types of products.
-        </p>
-        <p>Process includes guiding the customer through different categories of
-          products and services.  Website admins can customize the ordering process
-          with a drag-and-drop sortable list interface.  Created preset color schemes
-          and navbar shapes to provide customizable visual elements for non-developers.
-        </p>
+        <p>PhysicsSims is a collection of 2D physics simulations for aiding high school students, undergraduates, or the curious layman in learning physics.  By translating equations to real-time simlations with interactive controls, students can gain a deeper grasp of how the universe works without needing to obtain supplies to perform experiments.</p>
+        <p>The source code is open-source and is under active development. You can track my progress through <a href="http://github.com/chinibi/physics-sims" rel="noopener noreferrer" target="_blank">the GitHub repository</a>.</p>
       </div>
-    )
-  },
-  {
-    title: 'Access Cremation',
-    imageURL: '/images/accesscremation.jpg',
-    dates: 'February 2017',
-    tech: ['PHP', 'WordPress', 'JavaScript', 'WooCommerce'],
-    description: (
-      <div>
-        <p style={{fontSize: '1.2em'}}>This website is not yet online.</p>
-        <p>Created an e-commerce store for a California cremation service.  The
-          store takes users through a number of steps in a guided ordering process.</p>
-      </div>
-    )
-  },
-  {
-    title: 'Pacific Family Dental',
-    imageURL: '/images/pacificfamilydental.png',
-    dates: 'January 2017',
-    tech: ['JavaScript', 'Node.js', 'PostgreSQL', 'React', 'Redux'],
-    description: (
-      <div>
-        <p style={{fontSize: '1.2em'}}>This website is not yet online.</p>
-        <p>
-          An Orange County family dentistry site where patients can schedule their
-          appointments online.  This is a React SPA with a Node and Postgres API backend.
-          You may view the demo site by clicking the link above.
-        </p>
-      </div>
-    )
-  },
-  {
-    title: 'CSUN Jazz Studies',
-    imageURL: '/images/csunjazz.png',
-    dates: 'September 2016',
-    website: 'http://csunjazz.com',
-    tech: ['Wordpress', 'jQuery'],
-    description: (
-      <div>
-        <p>
-          Collaborated with Electronic Methods to redesign CSU Northridge's
-          Jazz Studies website.  Replaced images that served as simple backgrounds, borders,
-          and section dividers with CSS to greatly reduce page size.  Rendered the site
-          mobile-responsive with CSS media queries and jQuery.  Transplanted a
-          slideshow from another project to the site's front page.
-        </p>
-      </div>
-    )
-  },
-  {
-    title: 'Danny Boy Seeds',
-    imageURL: '/images/dannyboyseeds.png',
-    dates: 'July 2016',
-    website: 'http://dannyboyseeds.com',
-    tech: ['Materialize'],
-    description: (
-      <p>
-        My first freelancing project.  Danny Boy Seeds wholesales
-        vegetable seeds across the country.
-      </p>
     )
   },
   {
@@ -165,105 +98,240 @@ const projects = [
   }
 ];
 
-const Header = React.createClass({
-  render: function() {
-    return (
-      <div className="jumbotron text-center">
-        <div className="container">
-          <h2>Hello! My name is Trevor and I'm a web developer.</h2>
-        </div>
-
-        <div id="header-links" className="container">
-          <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank"><span className="fa fa-github"></span></a>
-          <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank"><span className="fa fa-linkedin"></span></a>
-        </div>
+const work = [
+  {
+    title: 'Step-By-Step Ordering System for WooCommerce',
+    imageURL: '/images/wc-step-by-step-ordering.png',
+    dates: 'July 2017',
+    website: 'http://stepbystepsys.com',
+    github: 'https://github.com/chinibi/wc-step-by-step-ordering-free',
+    tech: ['PHP', 'WordPress', 'JavaScript', 'WooCommerce'],
+    description: (
+      <div>
+        <p>Created a step-by-step ordering plugin that extends the WooCommerce WordPress library.
+          Customers are guided through a store by progressing through a number of steps each showing different types of products.
+        </p>
+        <p>Process includes guiding the customer through different categories of
+          products and services.  Website admins can customize the ordering process
+          with a drag-and-drop sortable list interface.  Created preset color schemes
+          and navbar shapes to provide customizable visual elements for non-developers.
+        </p>
       </div>
-    );
-  }
-});
-
-const ProjectList = React.createClass({
-  render: function() {
-
-    const projectList = this.props.projects.map((project, index) => {
-      return (
-        <ProjectListItem
-          key={index}
-          project={project}
-          className="list-inline-item"
-        />
-      );
-    });
-
-    return (
-      <div id="portfolio" className="container-fluid">
-        {projectList}
+    )
+  },
+  {
+    title: 'Access Cremation',
+    imageURL: '/images/accesscremation.jpg',
+    dates: 'February 2017',
+    tech: ['PHP', 'WordPress', 'JavaScript', 'WooCommerce'],
+    description: (
+      <div>
+        <p style={{fontSize: '1.2em'}}>This website is not yet online.</p>
+        <p>Created an e-commerce store for a California cremation service.  The
+          store takes users through a number of steps in a guided ordering process.</p>
       </div>
-    );
-
-  }
-});
-
-const ProjectListItem = React.createClass({
-
-  render: function() {
-    const {project: {
-      title, imageURL, dates, website, github, tech, description
-    }} = this.props;
-
-    const techList = tech.map((each, index) => {
-      return (
-        <li className="list-inline-item">
-          {each}
-        </li>
-      );
-    });
-
-    return (
-      <div className="clearfix project-entry">
-
-        <div className='col-sm-6'>
-          <img className="img-responsive img-thumbnail" src={imageURL} />
-        </div>
-
-        <div className='col-sm-6'>
-          {website ? <h2><a href={website} target="_blank">{title}</a></h2> : <h2>{title}</h2>}
-          {github ? <a href={github} target="_blank"><small>GitHub</small></a> : null}
-          <p>{dates}</p>
-          {description}
-          <ul className="list-inline">
-            {techList}
-          </ul>
-        </div>
-
+    )
+  },
+  {
+    title: 'Pacific Family Dental',
+    imageURL: '/images/pacificfamilydental.png',
+    dates: 'January 2017',
+    tech: ['JavaScript', 'Node.js', 'PostgreSQL', 'React', 'Redux'],
+    description: (
+      <div>
+        <p style={{fontSize: '1.2em'}}>This website is not yet online.</p>
+        <p>
+          An Orange County family dentistry site where patients can schedule their
+          appointments online.  This is a React SPA with a Node and Postgres API backend.
+          You may view the demo site by clicking the link above.
+        </p>
       </div>
-    );
-  }
-
-});
-
-const Footer = React.createClass({
-  render: function() {
-    return (
-      <div id='footer' className="container text-center">
-        <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a>
-        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a>
+    )
+  },
+  {
+    title: 'CSUN Jazz Studies',
+    imageURL: '/images/csunjazz.png',
+    dates: 'September 2016',
+    website: 'http://csunjazz.com',
+    tech: ['Wordpress', 'jQuery'],
+    description: (
+      <div>
+        <p>
+          Collaborated with Electronic Methods to redesign CSU Northridge's
+          Jazz Studies website.  Replaced images that served as simple backgrounds, borders,
+          and section dividers with CSS to greatly reduce page size.  Rendered the site
+          mobile-responsive with CSS media queries and jQuery.  Transplanted a
+          slideshow from another project to the site's front page.
+        </p>
       </div>
-    );
+    )
+  },
+  {
+    title: 'Danny Boy Consulting',
+    imageURL: '/images/dannyboyseeds.png',
+    dates: 'July 2016',
+    website: 'http://dannyboyseeds.com',
+    tech: ['Materialize'],
+    description: (
+      <p>
+        Danny Boy Consulting provides services to farmers and vegetable seed dealers both domestic and international.
+      </p>
+    )
   }
-});
+];
 
-const App = React.createClass({
-  render: function() {
+
+/************
+*
+* Components
+*
+*************/
+const {Component} = React;
+
+const Header = () => {
+  return (
+    <div className="jumbotron text-center">
+      <div className="container">
+        <h2>Hello! My name is Trevor and I'm a web developer.</h2>
+      </div>
+
+      <div id="header-links" className="container">
+        <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank"><span className="fa fa-github"></span></a>
+        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank"><span className="fa fa-linkedin"></span></a>
+      </div>
+    </div>
+  );
+}
+
+const NavBar = props => {
+  return (
+    <nav id="navbar" role="navigation">
+      <ul className="nav nav-pills">
+        <li className={props.activePage === 'About' ? 'active' : null} role="presentation" id="About" onClick={props.onNavClick}>About</li>
+        <li className={props.activePage === 'Work' ? 'active' : null} role="presentation" id="Work" onClick={props.onNavClick}>Work</li>
+        <li className={props.activePage === 'Interests' ? 'active' : null} role="presentation" id="Interests" onClick={props.onNavClick}>Interests</li>
+      </ul>
+    </nav>
+  );
+}
+
+class MainContainer extends Component {
+  constructor() {
+    super();
+    this.state = {activePage: 'About'};
+    this.onNavClick = this.onNavClick.bind(this);
+  }
+
+  onNavClick(e) {
+    this.setState({activePage: e.target.id});
+  }
+
+  renderMainContent() {
+    switch(this.state.activePage) {
+      case 'About':
+        return <AboutMe />;
+        break;
+      case 'Work':
+        return <ProjectList projects={work} />;
+        break;
+      case 'Interests':
+        return <ProjectList projects={interests} />;
+        break;
+      default:
+        return <ProjectList projects={projects} />;
+        break;
+    }
+  }
+
+  render() {
     return (
       <div>
-        <Header />
-        <ProjectList projects={projects} />
-        <Footer />
+        <NavBar activePage={this.state.activePage} onNavClick={this.onNavClick} />
+        {this.renderMainContent()}
       </div>
     );
   }
-});
+}
+
+const AboutMe = () => {
+  return (
+    <div className="container">
+      <h2>About Me</h2>
+      <p>I'm a web developer.</p>
+    </div>
+  );
+}
+
+const ProjectList = props => {
+  const projectList = props.projects.map((project, index) => {
+    return (
+      <ProjectListItem
+        key={index}
+        project={project}
+        className="list-inline-item"
+      />
+    );
+  });
+
+  return (
+    <div id="portfolio" className="container-fluid">
+      {projectList}
+    </div>
+  );
+}
+
+const ProjectListItem = props => {
+  const {project: {
+    title, imageURL, dates, website, github, tech, description
+  }} = props;
+
+  const techList = tech.map((each, index) => {
+    return (
+      <li className="list-inline-item">
+        {each}
+      </li>
+    );
+  });
+
+  return (
+    <div className="clearfix project-entry">
+
+      <div className='col-sm-6'>
+        <img className="img-responsive img-thumbnail" src={imageURL} />
+      </div>
+
+      <div className='col-sm-6'>
+        {website ? <h2><a href={website} target="_blank">{title}</a></h2> : <h2>{title}</h2>}
+        {github ? <a href={github} target="_blank"><small>GitHub</small></a> : null}
+        <p>{dates}</p>
+        {description}
+        <ul className="list-inline">
+          {techList}
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <div id='footer' className="container text-center">
+      <a className="btn btn-social-icon btn-github" href="https://github.com/chinibi" target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a>
+      <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/trevorpham" target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+      <Footer />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <App />,
